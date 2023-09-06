9b135aab4468feced5e9ea81d9d81c82ac675f58
Taqtile <taqtile@Dory.local>
2023-09-06 13:35:47 -0300

    Adding validation for passwords and tests

--- __tests__/input-validations.test.ts
+++ __tests__/input-validations.test.ts
@@ -1,21 +1,51 @@
 import { test, expect } from '@jest/globals';
-import { validateEmail } from '../utils/input-validations';
+import { validateEmail, validatePassword } from '../utils/input-validations';
 import { ValidationObject } from '../utils/input-validations';
 
-test('Has empty email invalidation', () => {
+test('Has an empty email invalidation', () => {
     const emptyEmail: string = '';
     let emailValidation: ValidationObject = validateEmail(emptyEmail);
     expect(emailValidation["isValidInput"]).toBe(false);
 });
 
-test('Has invalid email message', () => {
-    const emptyEmail: string = 'e@usp.com';
-    let emailValidation: ValidationObject = validateEmail(emptyEmail);
+test('Has an invalid email message', () => {
+    const invalidEmail: string = 'e@usp.com';
+    let emailValidation: ValidationObject = validateEmail(invalidEmail);
     expect(emailValidation["errorMessage"]).toBeDefined();
 });
 
 test('Has a valid email', () => {
-    const emptyEmail: string = 'taq@tiler.com';
-    let emailValidation: ValidationObject = validateEmail(emptyEmail);
+    const validEmail: string = 'taq@tiler.com';
+    let emailValidation: ValidationObject = validateEmail(validEmail);
     expect(emailValidation["isValidInput"]).toBe(true);
+});
+
+test('Has an empty password invalidation', () => {
+    const emptyPassword: string = '';
+    let passwordValidation: ValidationObject = validatePassword(emptyPassword);
+    expect(passwordValidation["isValidInput"]).toBe(false);
+});
+
+test('Has a too short password invalidation', () => {
+    const shortPassword: string = 'abc123';
+    let passwordValidation: ValidationObject = validatePassword(shortPassword);
+    expect(passwordValidation["isValidInput"]).toBe(false);
+});
+
+test('Has a password without digits)', () => {
+    const invalidPassword: string = 'abcdefgh';
+    let passwordValidation: ValidationObject = validatePassword(invalidPassword);
+    expect(passwordValidation["errorMessage"]).toBeDefined();
+});
+
+test('Has a password without letters', () => {
+    const invalidPassword: string = '1234567@';
+    let passwordValidation: ValidationObject = validatePassword(invalidPassword);
+    expect(passwordValidation["errorMessage"]).toBeDefined();
+});
+
+test('Has a valid password', () => {
+    const validPassword: string = 'abcd1234';
+    let passwordValidation: ValidationObject = validatePassword(validPassword);
+    expect(passwordValidation["isValidInput"]).toBe(true);
 });
--- utils/input-validations.ts
+++ utils/input-validations.ts
@@ -1,13 +1,18 @@
 const EMPTY_STRING_SIZE = 0; 
-const EMPTY_INPUT_MESSAGE = `O campo deve ser preenchido`;
+const MINIMUM_PASSWORD_SIZE = 7;
 
+const EMPTY_INPUT_MESSAGE = `O campo deve ser preenchido`;
 export interface ValidationObject {
-  errorMessage?: string;
   isValidInput: boolean;
+  errorMessage?: string;
 }
 
 function isInputEmpty(input: string): boolean {
   return input.length === 0 ? true : false;
+}
+
+function isInputTooShort(input: string, MINIMUM_PASSWORD_SIZE: number): boolean {
+  return input.length < MINIMUM_PASSWORD_SIZE ? true : false;
 }
 
 function isEmailPatternInvalid(email: string): boolean {
@@ -15,7 +20,11 @@ function isEmailPatternInvalid(email: string): boolean {
   return email.match(emailValidPattern) ? false : true;
 }
 
-export function validateEmail(email: string): ValidationObject{
+function isPasswordPatternInvalid(password: string): boolean {
+  return password.match(/[a-zA-Z]/) && password.match(/\d/) ? false : true;
+}
+
+export function validateEmail(email: string): ValidationObject {
   if (isInputEmpty(email)) {
     return {
       isValidInput: false,
@@ -31,3 +40,27 @@ export function validateEmail(email: string): ValidationObject{
     isValidInput: true,
   };
 }
+
+export function validatePassword(password: string): ValidationObject {
+  if (isInputEmpty(password)) {
+    return {
+      isValidInput: false,
+      errorMessage: EMPTY_INPUT_MESSAGE,
+    };
+  } else if (isInputTooShort(password, MINIMUM_PASSWORD_SIZE)) {
+    return {
+      isValidInput: false,
+      errorMessage: 'A senha é deve conter, no mínimo, 7 caracteres',
+    };
+  } else if (isPasswordPatternInvalid(password)) {
+    return {
+      isValidInput: false,
+      errorMessage: 'A senha deve conter, no mínimo, uma letra e um dígito',
+    }
+  }
+
+  return {
+    isValidInput: true,
+  }
+}
+

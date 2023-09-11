fdf6829584ce5fc9b79fa7e26d0523d08aef058e
Taqtile <taqtile@Dory.local>
2023-09-11 18:13:34 -0300

    Setting up Apollo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { LoginScreen } from './src/login-screen';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://template-onboarding-node-sjz6wnaoia-uc.a.run.app/graphql',
+  cache: new InMemoryCache(),
+});
 
 export function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <LoginScreen/>
-    </SafeAreaView>
+    <ApolloProvider client={client}>
+      <SafeAreaView>
+        <LoginScreen />
+      </SafeAreaView>
+    </ApolloProvider>
   );
 }

317d05bc4db21af259f73ecf53daa7d8a10fa890
Taqtile <taqtile@Dory.local>
2023-09-13 12:43:59 -0300

    index.js setup

--- index.js
+++ index.js
@@ -2,8 +2,25 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import { HomeScreen } from './src/home-screen';
 import { App } from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('Login', () => App);
+Navigation.registerComponent('Home', () => HomeScreen);
+
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Login'
+            }
+          }
+        ]
+      }
+    }
+  });
+});

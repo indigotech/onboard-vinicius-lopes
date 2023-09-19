4e6858e81994a318b4b0aeb4cce7ac73c1d2032b
Taqtile <taqtile@Dory.local>
2023-09-19 11:30:05 -0300

    Initial set up for user details page

--- src/components/users-list.tsx
+++ src/components/users-list.tsx
@@ -3,17 +3,18 @@ import { Item } from "./user-item";
 import { User } from "../hooks/use-get-users";
 
 interface Props {
+  navigationId: string;
   users: User[];
   fetchMore: () => void;
 }
 
-export function UsersList({users, fetchMore}: Props): JSX.Element {
+export function UsersList({navigationId, users, fetchMore}: Props): JSX.Element {
 
   return (
     <FlatList 
       data={users}
       keyExtractor={(item) => item.id}
-      renderItem={({item}) => <Item name={item.name} email={item.email} />}
+      renderItem={({item}) => <Item navigationId={navigationId} id={item.id} name={item.name} email={item.email} />}
       onEndReachedThreshold={0.4}
       onEndReached={fetchMore}
     />
